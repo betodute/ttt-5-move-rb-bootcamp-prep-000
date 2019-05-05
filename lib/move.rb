d371ecf0979b6c61a7065fcb5a80b6de0aa8381a
Beto Dute <betodute@gmail.com>
2019-05-05 04:00:46 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,17 @@
+./bin/move executing a CLI Application
+  defines a board variable (FAILED - 1)
 
+Failures:
+
+  1) ./bin/move executing a CLI Application defines a board variable
+     Failure/Error: board = get_variable_from_file("./bin/move", "board")
+     ArgumentError:
+       wrong number of arguments (given 0, expected 1)
+     # ./lib/move.rb:2:in `display_board'
+     # ./spec/spec_helper.rb:21:in `get_variable_from_file'
+     # ./spec/spec_helper.rb:11:in `eval'
+     # ./spec/spec_helper.rb:11:in `get_variable_from_file'
+     # ./spec/03_cli_spec.rb:9:in `block (2 levels) in <top (required)>'
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
